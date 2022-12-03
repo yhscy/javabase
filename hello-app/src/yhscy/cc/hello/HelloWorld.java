60c6fc9c21bcb11ac31dc0cdb68a5b55cebc3803
Admin <fangyuan0727@126.com>
2022-12-03 16:34:12 +0800

    第一个java程序

---
+++ hello-app/src/yhscy/cc/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package yhscy.cc.hello;
+
+/**
+ * @author ShiChun
+ * @date 2022/12/3 16:32
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Don't give up halfway");
+    }
+}
